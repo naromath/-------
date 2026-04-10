4606c72d72c55e58701a8b19de0e17753a01e431
naromath <jeongdo.son@gmail.com>
2026-04-10 13:19:39 +0900

    fix: base path 수정 및 Worker URL 환경변수 설정

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: '/순천은행-농기계-판독-시스템/',
+    base: '/-------/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
